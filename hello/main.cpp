5b8159cf08c546968c042999f6568395c5fa5b9b
Chip Collier <chip.collier@innogames.com>
2017-11-28 08:54:02 +0100

    bgfx and sdl2 scaffold

--- hello/main.cpp
+++ hello/main.cpp
@@ -28,11 +28,15 @@ window_ptr_t make_window(uint32_t width, uint32_t height)
     );
 }
 
-}
+} // namespace sdl2
 
-int init_bgfx(bgfx::RendererType::Enum renderer_type, sdl2::window_ptr_t& window)
+enum class result_t {
+  OK, ERR
+};
+
+result_t init_bgfx(bgfx::RendererType::Enum renderer_type, sdl2::window_ptr_t& window)
 {
-  int result = 0;
+  result_t result = result_t::OK;
 
   int width, height;
   SDL_GetWindowSize(window.get(), &width, &height);
@@ -60,7 +64,7 @@ int init_bgfx(bgfx::RendererType::Enum renderer_type, sdl2::window_ptr_t& window
 
   if (!bgfx::init(bgfx::RendererType::OpenGL)) {
     SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Failed to initialize bgfx!");
-    result = 1;
+    result = result_t::ERR;
   }
   else {
     bgfx::reset(width, height, BGFX_RESET_VSYNC);
@@ -70,37 +74,76 @@ int init_bgfx(bgfx::RendererType::Enum renderer_type, sdl2::window_ptr_t& window
       , 1.0f
       , 0
 			);
+    bgfx::setViewRect(0, 0, 0, uint16_t(width), uint16_t(height));
   }
 
   return result;
 }
 
-int main(int argc, char* argv[])
-{
-  int result = 0;
+result_t update(float delta) {
+  return result_t::OK;
+}
+
+result_t render() {
+  // dummy draw call
+  bgfx::touch(0);
+
+  bgfx::frame();
+
+  return result_t::OK;
+}
+
+result_t run() {
+  result_t status = result_t::OK;
 
   uint32_t width = 1280, height = 720;
-
-  auto init_flags = SDL_INIT_TIMER | SDL_INIT_EVENTS | SDL_INIT_GAMECONTROLLER | SDL_INIT_VIDEO;
-  result = SDL_Init(init_flags);
-  if (result != 0) {
-    SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Unable to initialize SDL: %s", SDL_GetError());
+  auto window = sdl2::make_window(width, height);
+  if (!window) {
+    SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Unable to create window! %s", SDL_GetError);
+    status = result_t::ERR;
   }
   else {
-    auto window = sdl2::make_window(width, height);
-    if (!window) {
-      SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Unable to create window! %s", SDL_GetError);
-    }
-    else {
-      result = init_bgfx(bgfx::RendererType::OpenGL, window);
-      if (result == 0) {
-        SDL_Delay(3000);
-        bgfx::shutdown();
+    status = init_bgfx(bgfx::RendererType::OpenGL, window);
+    if (status == result_t::OK) {
+      bgfx::frame();
+
+      bool exitRequested = false;
+      while (!exitRequested && status != result_t::ERR) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+          switch(event.type) {
+            case SDL_QUIT: {
+              exitRequested = true;
+            } break;
+          }
+        }
+
+        status = update(0.0f);
+        if (status == result_t::OK) {
+          status = render();
+        }
       }
+
+      bgfx::shutdown();
     }
+  }
+  return result_t::OK;
+}
+
+int main(int argc, char* argv[])
+{
+  result_t result = result_t::OK;
+
+  auto init_flags = SDL_INIT_TIMER | SDL_INIT_EVENTS | SDL_INIT_GAMECONTROLLER | SDL_INIT_VIDEO;
+  if (SDL_Init(init_flags) != 0) {
+    SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Unable to initialize SDL: %s", SDL_GetError());
+    result = result_t::ERR;
+  }
+  else {
+    result = run();
     SDL_Quit();
   }
 
 
-  return result;
+  return (int)result;
 }
