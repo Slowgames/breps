f005c7e43e3791faa3843e7fa767a0c231eb746d
Chip Collier <photex@gmail.com>
2017-12-14 10:44:50 +0100

    added basic cppcheck project; fixed compilation database generation script

--- generate_compilation_database.ps1
+++ generate_compilation_database.ps1
@@ -1,4 +1,4 @@
 
 bazel build -c opt compilation_database
 
-cat .\bazel-bin\compile_commands.json | sed -e "s,__EXEC_ROOT__,$(bazel info execution_root),g" > .\compile_commands.json
+cat .\bazel-bin\compile_commands.json | sed -e "s,__EXEC_ROOT__,$(bazel info workspace),g" > .\compile_commands.json
